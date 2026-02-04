a46de14fa6b47f124245f7ae7071fedd6c541b87
sanyamshahi <ersatishshahi@gmail.com>
2026-02-04 21:13:06 +0545

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,20 @@
+#1. Take inputs of names and marks from 3 students for their marks in Math 
+# , Science and English, Find the percentage and print the percentage for each student
+student_1=input("enter the name")
+math=float(input("enter your marks in math"))
+science=float(input("enter your marks in science"))
+english=float(input("enter your marks in english"))
+print(f"{student_1} has got {(math+science+english)/300*100}%")
+
+student_2=input("enter the name")
+math_1=float(input("enter your marks in math"))
+science_1=float(input("enter your marks in science"))
+english_1=float(input("enter your marks in english"))
+print(f"{student_2} has got {(math_1+science_1+english_1)/300*100}%")
+
+student_3=input("enter the name")
+math_2=float(input("enter yor marks in math"))
+science_2=float(input("enter your marks in science"))
+english_2=float(input("enter your marks in english"))
+print(f"{student_3} has got {(math_2+science_2+english_2)/300*100}%")
+
