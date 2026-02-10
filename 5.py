5ba179067377dff00a7af9932c82705121c744ba
sanyamshahi <ersatishshahi@gmail.com>
2026-02-10 21:11:15 +0545

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,15 @@
+num = input("Enter a number: ")
+
+if num == num[::-1]:
+    print("The number is a palindrome")
+else:
+    print("The number is not a palindrome")
+    
+    #for word
+
+word = input("Enter a word: ")
+
+if word == word[::-1]:
+    print("The word is a palindrome")
+else:
+    print("The word is not a palindrome")
---
+++ new.py
@@ -0,0 +1,13 @@
+name="sanyam"
+age=15
+tempreature=30.5 
+x=False
+ 
+#tempreature=int(tempreature)
+#print(type(tempreature))
+#age=str(age)
+#print(type(age))
+word="Naman"
+
+#rome
+print(word[-5:])
